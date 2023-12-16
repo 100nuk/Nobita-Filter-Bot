9992771c855dd572c61bd38daa73ee08800e861e
100nuk <146798173+100nuk@users.noreply.github.com>
2023-12-16 18:22:57 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import logging
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
-from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM, SHORTLINK_URL, SHORTLINK_API, LOG_CHANNEL, ADMINS, REQ_CHANNEL, TUTORIAL, CUSTOM_FILE_CAPTION, IS_VERIFY, VERIFY2_URL, VERIFY2_API
+from info import *
 from database.join_reqs import JoinReqs as db2
 from imdb import Cinemagoer 
 import asyncio
